84868e34ff977df6828efea8ceec0469838c9fce
Adrian <110757740+aclrians@users.noreply.github.com>
2025-06-19 16:24:54 -0700

    Can now improve user body of text

--- main.py
+++ main.py
@@ -17,24 +17,28 @@ reddit = praw.Reddit(
 # Loading openai API key
 client = OpenAI(api_key=os.getenv("OPEN_AI_KEY"))
 
+subName = input("Hello, enter the name of the subreddit you'd like to post in :\n")
+title = input("Now, name your post :\n")
+body = input("Enter your main content here:\n")
+
+
+
+
 # Sending a message 
 response = client.chat.completions.create(
     model = "gpt-3.5-turbo",
     messages = [
-        {"role":"system", "content": "You are an experienced poster on Reddit. You know how to spot virality and trends which you use to enhance your posts"},
-        {"role":"user","content": "Write a reddit post about the dangers of increased screen time in less than 4 paragraphs"}
+        {"role":"system", "content": "You are an experienced poster on Reddit. You know how to spot virality and trends which you use to enhance your posts. When a user sends you a prompt, its their body paragraph, therefore, your job is to transform that body and maximize its potential as a post."},
+        {"role":"user","content": f"Improve the quality and virality of {body}"}
 
     ],
     temperature = 0.7,
     max_tokens = 500
 )
 
-print(response.choices[0].message.content)
+body = (response.choices[0].message.content)
 
 
-subName = input("Hello, enter the name of the subreddit you'd like to post in :\n")
-title = input("Now, name your post :\n")
-body = input("Enter your body of text: ")
 
 
 
